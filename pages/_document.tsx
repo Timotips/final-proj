89ba17b07796c4c1123b7dfdc3406b9dbd8507c6
Timotips <kingbam17@gmail.com>
2023-01-23 08:40:36 +0800

    crossorigin error fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@300;400&family=Inter:wght@300;400&family=Josefin+Sans:wght@300;400&family=Old+Standard+TT&family=Open+Sans:wght@300;400&family=Poppins:wght@300;400&family=Quicksand:wght@300;400&family=Slabo+13px&display=swap" rel="stylesheet" />
       </Head>
       <body>
